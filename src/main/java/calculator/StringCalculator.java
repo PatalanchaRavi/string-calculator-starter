1ad413d77e5f5a86dcf922e2e4f9207dac886717
Ravindra Patil <rp0654031@gmail.com>
2021-10-16 18:56:24 +0530

    add method added in calculator class and sum and splitNumber method decleard

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,29 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	if(input.equals("")){
+			return 0;
+		}
+		else{
+			String delimiter = ",";
+			if(input.matches("//(.*)\n(.*)")){
+				delimiter = Character.toString(text.charAt(2));
+				input = input.substring(4);
+			}
+			
+			String numList[] = splitNumbers(input, delimiter + "|\n");
+			return sum(numList);
+		}
     }
+
+	private int sum(String[] numList) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private String[] splitNumbers(String input, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
